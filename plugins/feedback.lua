c202a961d031b4c3a2ab6ef7f078b48df430bf09
hacker4moon <0.creed.is.dead.0@gmail.com>
2015-12-31 18:10:10 +0330

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -2,30 +2,41 @@ do
 
 function run(msg, matches)
 
-local fuse = '#DearAdmin now i have recived new #Feedback😝 \n\nid : ' .. msg.from.id .. '\n\nName : ' .. msg.from.print_name ..'\n\nLetters :\n\n\n' .. matches[1] 
+local fuse = '#DearAdmin😜 we have recived a new feedback just now : #newfeedback \n\nid : ' .. msg.from.id .. '\n\nName : ' .. msg.from.print_name ..'\n\nusername : ' .. msg.from.username ..'\n\nLetters :\n\n\n' .. matches[1] 
 local fuses = '!printf user#id' .. msg.from.id
 
 
- local text = matches[1]
+    local text = matches[1]
+ bannedidone = string.find(msg.from.id, '123')
+        bannedidtwo =string.find(msg.from.id, '465')       
+   bannedidthree =string.find(msg.from.id, '678')  
 
- local sends = send_msg('chat#id..70690378', fuse, ok_cb, false)
 
- return 'پیام شما به درستی رسید�!!'
+        print(msg.to.id)
 
- 
+        if bannedidone or bannedidtwo or bannedidthree then                    —for banned people
+                return 'You are banned to send a feedback'
+ else
+
+
+                 local sends0 = send_msg('chat#70690378', fuse, ok_cb, false)
+
+ return 'پیام شما به درستی تحویل داده شد�!'
+
+     
 
 end
 
 end
-
 return {
- 
- description = "برای مدیر نظراتتون رو بفرستین",
+  description = "Feedback",
 
- usage = "!feedback <matn> : نظراتتون رو و یا هر چیزی که خواستین بگین رو توسط این پلاگین میتونین به ادمین برسونین فقط کافیه بجای عبارت matn  متن خودتون رو بنویسید",
- patterns = {
- "^![Ff]eedback (.*)$"
+  usage = "!feedback <matn> : پیامتان را که میخواهید مدیر یا سازندذه ربات ببینه در فرمول رو به رو در جای عبارت matn  جایگزاری کرده و به پیوی ربات یا گروه آن بفرستید",
+  patterns = {
+    "^![Ff]eedback (.*)$"
 
- },
- run = run
+  },
+  run = run
 }
+
+end
