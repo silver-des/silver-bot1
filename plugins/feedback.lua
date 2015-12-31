536ce519232f00775a1904747f2f1b5cdb452f48
hacker4moon <0.creed.is.dead.0@gmail.com>
2015-12-31 18:58:41 +0330

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -2,7 +2,7 @@ do
 
 function run(msg, matches)
 
-local fuse = '#DearAdmin😜 we have recived a new feedback just now : #newfeedback \n\nid : ' .. msg.from.id .. '\n\nName : ' .. msg.from.print_name ..'\n\nusername : ' .. msg.from.username ..'\n\nLetters :\n\n\n' .. matches[1] 
+local fuse = '#DearAdmin😜 we have recived a new feedback just now : #newfeedback \n\nID▶️ : ' .. msg.from.id .. '\n\nName▶ : ' .. msg.from.print_name ..'\n\nusername▶️ :@ ' .. msg.from.username  ..'\n\n🅿️♏️ :\n\n\n' .. matches[1] 
 local fuses = '!printf user#id' .. msg.from.id
 
 
@@ -14,14 +14,14 @@ local fuses = '!printf user#id' .. msg.from.id
 
         print(msg.to.id)
 
-        if bannedidone or bannedidtwo or bannedidthree then                    —for banned people
+        if bannedidone or bannedidtwo or bannedidthree then                    --for banned people
                 return 'You are banned to send a feedback'
  else
 
 
                  local sends0 = send_msg('chat#70690378', fuse, ok_cb, false)
 
- return 'پیام شما به درستی تحویل داده شد�!'
+ return 'your feedback succesfully recived😜!'
 
      
 
@@ -31,7 +31,7 @@ end
 return {
   description = "Feedback",
 
-  usage = "!feedback <matn> : پیامتان را که میخواهید مدیر یا سازندذه ربات ببینه در فرمول رو به رو در جای عبارت matn  جایگزاری کرده و به پیوی ربات یا گروه آن بفرستید",
+  usage = "!feedback : send maseage to admins with bot",
   patterns = {
     "^![Ff]eedback (.*)$"
 
